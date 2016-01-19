4ccbb5b3fc2cb35faf55c637ba7def78619ba119
Alex Dean <alex@crackpot.org>
2016-01-19 16:28:08 -0600

    any 4.1 or 4.2 should be ok.
    i should set up travis to verify this on an ongoing basis. for now, i manually
    updated to 4.1.1, 4.1.5, and 4.2.5. all specs pass on those versions.

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -19,8 +19,8 @@ module Dummy
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
+    # Do not swallow errors in after_commit/after_rollback callbacks. (added in 4.2)
+    # config.active_record.raise_in_transactional_callbacks = true
   end
 end
 
