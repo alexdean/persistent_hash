de1848e4b4e2f95b0d3597cade424ba9f07afda5
Alex Dean <github@mostlyalex.com>
2019-06-18 10:56:24 -0500

    more tidying up test configs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,8 +5,6 @@ require File.expand_path("../dummy/config/environment", __FILE__)
 require 'rspec/rails'
 require 'timecop'
 
-puts Rails.version
-
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -21,10 +19,8 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
-
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
-
 end
