8c0dc928e89ec880a4a0b28b6c9b013de3a7926b
Alex Dean <alexdean@users.noreply.github.com>
2026-06-08 17:44:46 -0500

    run test suite with rails 7 (#8)
    * test against rails 7
    
    * forgot this was copypasta. need to DRY this at some point.
    
    * rails 7 requires at least ruby 2.7

--- spec/dummy/config/initializers/persistent_hash.rb
+++ spec/dummy/config/initializers/persistent_hash.rb
@@ -1,9 +1,11 @@
-PersistentHash::Formatter.add(
-  ActiveSupport::TimeWithZone,
-  ->(val) { val.iso8601 }
-)
+Rails.application.config.to_prepare do
+  PersistentHash::Formatter.add(
+    ActiveSupport::TimeWithZone,
+    ->(val) { val.iso8601 }
+  )
 
-PersistentHash::Formatter.add(
-  Time,
-  ->(val) { val.iso8601 }
-)
+  PersistentHash::Formatter.add(
+    Time,
+    ->(val) { val.iso8601 }
+  )
+end
